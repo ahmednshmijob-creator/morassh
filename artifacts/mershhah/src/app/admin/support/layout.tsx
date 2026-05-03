0d1f9186b9284106429b7939cbdec9623889cccd
ahmednshmijob-creator <ahmednshmi.job@gmail.com>
2026-05-03 13:05:44 +0300

    fix: remove SidebarTrigger, fix support layout RTL border, clean header

--- artifacts/mershhah/src/app/admin/support/layout.tsx
+++ artifacts/mershhah/src/app/admin/support/layout.tsx
@@ -1,4 +1,3 @@
-
 'use client';
 import { ChatList } from '@/components/admin/support/ChatList';
 import { Card } from '@/components/ui/card';
@@ -9,8 +8,8 @@ export default function SupportLayout({
   children: React.ReactNode;
 }) {
   return (
-    <Card className="h-[calc(100vh-8.5rem)] md:grid md:grid-cols-3 xl:grid-cols-4 overflow-hidden">
-      <div className="hidden md:block border-l">
+    <Card className="h-[calc(100vh-8.5rem)] md:grid md:grid-cols-3 xl:grid-cols-4 overflow-hidden" dir="rtl">
+      <div className="hidden md:block border-s">
         <ChatList />
       </div>
       <div className="md:col-span-2 xl:col-span-3 h-full">
